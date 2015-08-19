d7244dd6dc6b54c8b2461cddb031dfc8f6380a28
Thejesh GN <i@thejeshgn.com>
2015-08-19 21:36:09 +0530

    Create md.lua

---
+++ md.lua
@@ -0,0 +1,7 @@
+local md = require('webscriptio/lib/markdown.lua')
+local domain = request.query.d
+local response = http.request {
+	url = 'http://'..domain
+}
+local response_html = markdown(response)
+return response_html, {["Cache-Control"]="no-cache", ["Content-Type"]="text/html"}
