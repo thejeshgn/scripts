02b5e663fd516ad3a5dfb818339cd08a98b5269f
Thejesh GN <i@thejeshgn.com>
2015-08-19 21:44:08 +0530

    Update md.lua

--- md.lua
+++ md.lua
@@ -1,4 +1,8 @@
 local md = require('markdown.lua')
 local d = request.query.d
-local response_html = markdown(d)
+local response = http.request {
+	url = 'http://'..d
+}
+
+local response_html = markdown(response)
 return response_html, {["Cache-Control"]="no-cache", ["Content-Type"]="text/html"}
