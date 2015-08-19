9c7d043ab9fb650936f8876f7896e740f980fc75
Thejesh GN <i@thejeshgn.com>
2015-08-19 21:59:03 +0530

    Update md.lua

--- md.lua
+++ md.lua
@@ -1,8 +1,8 @@
 local md = require('markdown.lua')
 local domain = request.query.d
 local response = http.request {url = 'http://'..domain}
---if response == nil or response.statuscode ~= 200 then
---  return "Markdown was not found", {["Cache-Control"]="no-cache", ["Content-Type"]="text/html"}
---end
+if response == nil or response.statuscode ~= 200 then
+  return "Markdown was not found", {["Cache-Control"]="no-cache", ["Content-Type"]="text/html"}
+end
 local response_html = markdown(response.content)
 return response_html, {["Cache-Control"]="no-cache", ["Content-Type"]="text/html"}
